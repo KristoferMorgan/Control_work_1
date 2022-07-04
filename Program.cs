4b66983fd034af71e071c5a1667b010098463a0b
Кристофер Морган <rezielsaw@gmail.com>
2022-07-05 01:15:17 +1000

    the method of solving the problem is described

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
 ﻿void PrintArray(string[] array)
 {
-   for(int i = 0,i < rowsLength,i++ )
+   for(int i = 0;i < array.GetLength(0);i++ )
       Console.WriteLine(array[i] + " ,");
+}
+void FillArray(string[]array)
+{
+    for(int i = 0;i < array.GetLength(0);i++)
+    {
+        Console.WriteLine("enter the {i} element of the array");
+        array[i] = Console.ReadLine();
+    }
+}
+void CopyArray(string[] array,string[]arrayCopy )
+{
+    int index = 0;
+    for(int i = 0;i < array.GetLength(0);i++)
+        if(array[i].Length < 4 )
+        {
+            arrayCopy[index] = array[i];
+            index++;
+        }
+
 }
